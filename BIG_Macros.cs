3bb87ca01dbd55dd01dc820a6ed9369a289212ad
Deyan Nenov <dnenov@archilizer.com>
2018-04-29 13:02:17 +0100

    Update BIG_Macros.cs

--- BIG_Macros.cs
+++ BIG_Macros.cs
@@ -661,7 +661,58 @@ namespace BIG_Macros
                 }
 			}
 		}
-		
+	
+		public void DimGrids()
+		{			
+			UIDocument uidoc = this.ActiveUIDocument;
+			Document doc = uidoc.Document;
+			
+			GridSelectionFilter filter = new ThisApplication.GridSelectionFilter(doc);			
+			IList<Element> grids = uidoc.Selection.PickElementsByRectangle(filter, "Pick Grid Lines");
+			
+			ReferenceArray refArray = new ReferenceArray();
+			XYZ dir = null;
+			
+			foreach(Element el in grids)
+			{
+				Grid gr = el as Grid;
+				
+				if(gr == null) continue;
+				if(dir == null)
+				{
+					Curve crv = gr.Curve;
+					dir = new XYZ(0,0,1).CrossProduct((crv.GetEndPoint(0) - crv.GetEndPoint(1)));
+				}
+				Reference gridRef = null;
+				Options opt = new Options();
+				opt.ComputeReferences = true;
+				opt.IncludeNonVisibleObjects = true;
+				opt.View = doc.ActiveView;
+				foreach (GeometryObject obj in gr.get_Geometry(opt))
+				{
+					if (obj is Line)
+					{
+						Line l = obj as Line;
+						gridRef = l.Reference;
+						refArray.Append(gridRef);
+					}
+				}
+			}
+			
+			XYZ pickPoint = uidoc.Selection.PickPoint();
+			Line line = Line.CreateBound(pickPoint, pickPoint + dir * 100);
+			
+			using(Transaction t = new Transaction(doc, "Make Dim"))
+			{
+				t.Start();
+				if( !doc.IsFamilyDocument )
+				{
+					doc.Create.NewDimension( 
+					  doc.ActiveView, line, refArray);
+				}				
+				t.Commit();
+			}					
+		}	
 	public void SelectUniqueModelGroups()
 		{
 			UIDocument uidoc = ActiveUIDocument;
